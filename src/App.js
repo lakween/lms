88ce424ad50154285198fe7a214d1cb404cf069e
lakweensenathilake <lakweensenathilake@gmail.com>
2022-06-12 10:07:22 +0530

    add firebase and boostrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Card} from "react-bootstrap";
 
 function App() {
   return (
@@ -17,6 +18,35 @@ function App() {
         >
           Learn React
         </a>
+        <>
+          {[
+            'Primary',
+            'Secondary',
+            'Success',
+            'Danger',
+            'Warning',
+            'Info',
+            'Light',
+            'Dark',
+          ].map((variant) => (
+              <Card
+                  bg={variant.toLowerCase()}
+                  key={variant}
+                  text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+                  style={{ width: '18rem' }}
+                  className="mb-2"
+              >
+                <Card.Header>Header</Card.Header>
+                <Card.Body>
+                  <Card.Title>{variant} Card Title </Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+          ))}
+        </>
       </header>
     </div>
   );
---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBb8tXHQJJtsUhmoyUfG08PLELe-haNO44",
+    authDomain: "lms-pro-cf49c.firebaseapp.com",
+    projectId: "lms-pro-cf49c",
+    storageBucket: "lms-pro-cf49c.appspot.com",
+    messagingSenderId: "815260992853",
+    appId: "1:815260992853:web:1894b21a4ca9fcd464dbdc",
+    measurementId: "G-BLBQGWQ49G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
