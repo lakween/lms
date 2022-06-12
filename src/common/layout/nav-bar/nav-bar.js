88b50adc7f049084fe53bd2e325c6c3330044965
lakweensenathilake <lakweensenathilake@gmail.com>
2022-06-12 12:42:23 +0530

    feat:navbar

--- src/common/layout/nav-bar/nav-bar.js
+++ src/common/layout/nav-bar/nav-bar.js
@@ -1,19 +1,17 @@
 import {Container, Nav, Navbar} from "react-bootstrap";
 
-const NavBar = ()=>{
-    return(
-        <><>
+const NavBar = () => {
+    return (
+        <>
             <Navbar bg="dark" variant="dark">
-                <div className={"ml-2 d-flex p-2 flex-row"}>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                </div>
+                <Navbar.Brand className={'ms-2'} href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
             </Navbar>
-        </></>
+        </>
     )
 }
 
