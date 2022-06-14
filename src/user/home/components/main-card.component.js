fdb26cebbcda856e50e3fb65eb6c8b2dbff81bd8
lakweensenathilake <lakweensenathilake@gmail.com>
2022-06-15 01:46:36 +0530

    fix

--- src/user/home/components/main-card.component.js
+++ src/user/home/components/main-card.component.js
@@ -2,16 +2,17 @@ import {Box, Flex, useColorModeValue, Wrap} from "@chakra-ui/react";
 
 const MainCard = ({children, ...rest}) => {
     return (
-        <Flex
+        <Box
+            overflowY={'scroll'}
             bg={useColorModeValue('white', 'gray.900')}
             borderWidth="2px"
             padding={'10px'}
             borderColor={useColorModeValue('gray.200', 'gray.700')}
             borderStyle={'solid'}
             {...rest}
-        >
-            {children}
-        </Flex>
+        className={"d-flex justify-content-between flex-wrap"}>
+         {children}
+        </Box>
     )
 }
 
--- src/user/home/home.page.js
+++ src/user/home/home.page.js
@@ -2,42 +2,18 @@ import MainCard from "./components/main-card.component";
 import {Center, useColorModeValue, Wrap, WrapItem} from "@chakra-ui/react";
 import CourseCardComponent from "./components/course-card.component";
 
-const HomePage = ()=>{
+const HomePage = () => {
 
     return (
-        <MainCard
-                  >
-           <Wrap spacing='30px'>
-               <WrapItem>
-                   <CourseCardComponent/>
-               </WrapItem>
-               <WrapItem>
-                   <CourseCardComponent/>
-               </WrapItem>
-               <WrapItem>
-                   <CourseCardComponent/>
-               </WrapItem>
-               <WrapItem>
-                   <CourseCardComponent/>
-               </WrapItem>
-               <WrapItem>
-                   <CourseCardComponent/>
-               </WrapItem>
-               <WrapItem>
-                   <CourseCardComponent/>
-               </WrapItem>
-               <WrapItem>
-                   <CourseCardComponent/>
-               </WrapItem>
-               <WrapItem>
-                   <CourseCardComponent/>
-               </WrapItem>
-               <WrapItem>
-                   <CourseCardComponent/>
-               </WrapItem>
-
-           </Wrap>
-            </MainCard>
+        <MainCard maxHeight={'88vh'}>
+            <CourseCardComponent/>
+            <CourseCardComponent/>
+            <CourseCardComponent/>
+            <CourseCardComponent/>
+            <CourseCardComponent/>
+            <CourseCardComponent/>
+            <CourseCardComponent/>
+        </MainCard>
 
     )
 }
