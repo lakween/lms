069e772f889560d715c77dcd9376021e3c290c1d
lakweensenathilake <lakweensenathilake@gmail.com>
2022-06-21 01:56:16 +0530

    feat:edit component

--- src/common/display-line/display-line.component.js
+++ src/common/display-line/display-line.component.js
@@ -7,10 +7,14 @@ const DisplayLine = ({text}) => {
     const [isEdit, setIsEdit] = useState(false)
 
     return (
-        <> {!isEdit ? (<Flex gap={5} direction={'row'} align={'center'}>
-            <Text mt={2}>{text}</Text>
-            <FaPencilAlt onClick={()=>setIsEdit(true)}/>
-        </Flex>) : <EditComponent setIsEdit={setIsEdit}/>} </>
+        <> {!isEdit ?
+            (
+                <Flex gap={5} marginY={2} direction={'row'} align={'center'}>
+                    <Text>{text}</Text>
+                    <FaPencilAlt onClick={() => setIsEdit(true)}/>
+                </Flex>
+            ) :
+            <EditComponent value={text} setIsEdit={setIsEdit}/>} </>
     )
 }
 
--- src/common/edite.componet.js
+++ src/common/edite.componet.js
@@ -1,12 +1,21 @@
 import {useSelector} from "react-redux";
 import {FaPencilAlt} from "react-icons/fa";
+import {Button, Flex} from "@chakra-ui/react";
+import {useState} from "react";
 
-const EditComponent = ({setIsEdit})=>{
-    let commonState = useSelector(state=>state)
-    console.log(commonState,'commonState')
+const EditComponent = ({setIsEdit,value}) => {
+    // let commonState = useSelector(state => state)
+    // console.log(commonState, 'commonState')
+    const [state,setState]= useState(value)
     return (
         <>
-            <input/>
+            <Flex marginY={2} gap={2}>
+                <input value={state} type={'text'} onChange={(e)=>{setState(e.target.value)}}/>
+                <Button onClick={() => setIsEdit(false)} colorScheme='teal' size='xs'>
+                   OK
+                </Button>
+            </Flex>
+
         </>
     )
 }
