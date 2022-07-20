9b255893032c1c56713a3090eed29ce8ae9c94c1
lakweensenathilake <lakweensenathilake@gmail.com>
2022-07-20 19:29:41 +0530

    feat:add choise sing type modal

---
+++ src/common/loging/components/modal/choise-signin-type.modal.jsx
@@ -0,0 +1,39 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton, Button,
+} from '@chakra-ui/react'
+import {useSelector} from "react-redux";
+
+const ChoiseSigninTypeModal = ({modalMethod,data})=>{
+
+    const { isOpen, onOpen, onClose } = modalMethod
+    return (
+        <>
+            <Button onClick={onOpen}>Open Modal</Button>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        sdhfvsdhgvfhd
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+}
+
+export default ChoiseSigninTypeModal
