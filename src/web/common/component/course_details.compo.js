6f5984b2d66454e521c958ceade0b33cca8c46c4
Udaya <unzoysa.un@gmail.com>
2022-07-24 15:52:47 +0530

    fleate : paymet slip and card added

--- src/web/common/component/course_details.compo.js
+++ src/web/common/component/course_details.compo.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import {
   Box,
   chakra,
@@ -15,11 +17,106 @@ import {
   VisuallyHidden,
   List,
   ListItem,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  useToast,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import useFormController from "../../../hooks/useFormController";
+import firebase from "firebase/compat/app";
+import { getStorage, ref, uploadBytes,getDownloadURL } from "firebase/storage";
+import { collection, addDoc } from "firebase/firestore";
+import useUserLoginInfo from "../../../hooks/useUserLoginInfo";
 
 export default function CourseDetails(props) {
+  let dispatch = useDispatch();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  let [valueChangeHandler, setValue, form, setForm] = useFormController();
+  const [image, setImage] = useState("");
+  const user = useUserLoginInfo();
+  const toast = useToast();
+
+  const stuID = user[2].uid;
+  const courseID = props.cid;
+
+  const handleImageAsFile = (e) => {
+    // Create a root reference
+
+    const image = e.target.files[0];
+    setImage((imageFile) => image);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log("start of upload");
+
+    const storage = getStorage();
+    const fileRef = ref(storage, `userPaymentSlip/${image.name}`);
+    const snapshot = await uploadBytes(fileRef, image);
+    const photoURL = await getDownloadURL(fileRef);
+
+    if (!form.st_pay_ref & !form.card_number) {
+      toast({
+        title: "plase try again !",
+        description: "Check your card detaile and try again !",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      const db = firebase.firestore();
+      const docRef = await addDoc(collection(db, "courseByStudent"), {
+        StudentID: "accounts/" + stuID,
+        CourseID: "courses/" + courseID,
+        isEnrolled: "true",
+        paymentSlip: photoURL,
+        payMethod: form.st_pay_ref ? "online" : "slip",
+        isPaid: "true",
+      });
+      if (docRef.id) {
+        toast({
+          title: "Your message has been submitted👍",
+          description: "Our representive contact you soon!",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "Something wrong",
+          description: "db error please check your connection",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    }
+    setForm("");
+  };
+
   const navigate = useNavigate();
+
+  const signedButtonMarkup = (
+    <Button colorScheme="blue" onClick={submitHandler}>
+      Proceess Next
+    </Button>
+  );
 
   return (
     <Container maxW={"7xl"}>
@@ -68,10 +165,8 @@ export default function CourseDetails(props) {
               />
             }
           >
-            <VStack spacing={{ base: 4, sm: 6 }}>       
-              <Text fontSize={"lg"}>
-                {props.desc}
-              </Text>
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <Text fontSize={"lg"}>{props.desc}</Text>
             </VStack>
             <Box>
               <Text
@@ -85,16 +180,15 @@ export default function CourseDetails(props) {
               </Text>
 
               <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-              <List spacing={2}>
-                    <ListItem>Writing</ListItem>
-                    <ListItem>Reading</ListItem>{' '}
-                    <ListItem>Speaking</ListItem>
-                  </List>
-                  <List spacing={2}>
-                    <ListItem>Listening</ListItem>
-                    <ListItem>Communication</ListItem>
-                    <ListItem>Team work</ListItem>
-                  </List>
+                <List spacing={2}>
+                  <ListItem>Writing</ListItem>
+                  <ListItem>Reading</ListItem> <ListItem>Speaking</ListItem>
+                </List>
+                <List spacing={2}>
+                  <ListItem>Listening</ListItem>
+                  <ListItem>Communication</ListItem>
+                  <ListItem>Team work</ListItem>
+                </List>
               </SimpleGrid>
             </Box>
             <Box>
@@ -109,40 +203,38 @@ export default function CourseDetails(props) {
               </Text>
 
               <List spacing={2}>
-                  
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Commitment:
-                    </Text>{' '}
-                    6 months
-                  </ListItem>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Exam Format:
-                    </Text>{' '}
-                    All three tests in the suite are designed to test the four core language skills (reading, writing, speaking and listening)
-                  </ListItem>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Commitment:
+                  </Text>{" "}
+                  6 months
+                </ListItem>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Exam Format:
+                  </Text>{" "}
+                  All three tests in the suite are designed to test the four
+                  core language skills (reading, writing, speaking and
+                  listening)
+                </ListItem>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
                     Scoring:
-                    </Text>{' '}
-                     Scoring above 50% is pass level.
-                  </ListItem>
-                  <ListItem>
-                    <Text as={'span'} fontWeight={'bold'}>
-                      Certificate:
-                    </Text>{' '}
-                    All pass candidates scoring receive a certificate.
-                  </ListItem>
-                  
-                </List>
+                  </Text>{" "}
+                  Scoring above 50% is pass level.
+                </ListItem>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Certificate:
+                  </Text>{" "}
+                  All pass candidates scoring receive a certificate.
+                </ListItem>
+              </List>
             </Box>
           </Stack>
 
           <Button
-            onClick={() => {
-              navigate("/coursepayment");
-            }}
+            onClick={onOpen}
             rounded={"none"}
             w={"full"}
             mt={8}
@@ -158,6 +250,123 @@ export default function CourseDetails(props) {
           >
             ENROLL NOW
           </Button>
+
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Course Payment</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Tabs>
+                  <TabList>
+                    <Tab>Upload Payment Slip</Tab>
+                    <Tab>Online Payment</Tab>
+                  </TabList>
+                  <TabPanels>
+                    <TabPanel>
+                      <FormControl>
+                        <FormLabel>Payment Reference Number</FormLabel>
+                        <Input
+                          placeholder="Upload Your Bank Pay Slip"
+                          type="text"
+                          name="st_pay_ref"
+                          id="st_pay_ref"
+                          onChange={valueChangeHandler}
+                          value={form.st_pay_ref}
+                        />
+                      </FormControl>
+                      <FormControl mt={4}>
+                        <FormLabel>Slip</FormLabel>
+                        <Input
+                          placeholder="Upload Your Bank Pay Slip"
+                          type={"file"}
+                          name="file"
+                          id="file"
+                          onChange={handleImageAsFile}
+                        />
+                        <FormHelperText>
+                          Note : Upload your payment slip here with student Name
+                        </FormHelperText>
+                      </FormControl>
+                    </TabPanel>
+                    <TabPanel>
+                      <Stack direction="coloum">
+                        <Image
+                          objectFit="cover"
+                          src="https://www.payhere.lk/downloads/images/payhere_square_banner.png"
+                          alt="PayHere"
+                        />
+                      </Stack>
+                      <Stack alignItems="center" pt="4">
+                        <Heading as="h4" size="md">
+                          Add Credit Card
+                        </Heading>
+                        <FormControl>
+                          <FormLabel>Card Holder Name</FormLabel>
+                          <Input
+                            type="text"
+                            name="cardholder_name"
+                            id="cardholder_name"
+                            placeContent={"David Beckem"}
+                            onChange={valueChangeHandler}
+                          />
+                        </FormControl>
+                        <FormControl isInvalid={form.card_number}>
+                          <FormLabel>Card Number</FormLabel>
+                          <Input
+                            type="number"
+                            onChange={valueChangeHandler}
+                            name="card_number"
+                          />
+                          {!form.cardName ? (
+                            <FormHelperText>
+                              Card Verify Checking . . .
+                            </FormHelperText>
+                          ) : (
+                            <FormErrorMessage>
+                              creadit card invalid !
+                            </FormErrorMessage>
+                          )}
+                        </FormControl>
+                        <Stack spacing={8} direction="row">
+                          <FormControl isRequired>
+                            <FormLabel>Expiration Date</FormLabel>
+                            <Input
+                              placeholder="First name"
+                              type={"date"}
+                              onChange={valueChangeHandler}
+                              name="expire_date"
+                            />
+                          </FormControl>
+                          <FormControl isRequired isInvalid={form.csv_pin}>
+                            <FormLabel>CSV Pin</FormLabel>
+                            <Input
+                              placeholder="ex: 9xx"
+                              type={"number"}
+                              limit="3"
+                              onChange={valueChangeHandler}
+                              name="csv_pin"
+                            />
+                            {!form.cardPin ? (
+                              <FormHelperText>
+                                Card Verify Checking . . .
+                              </FormHelperText>
+                            ) : (
+                              <FormErrorMessage>
+                                creadit card invalid !
+                              </FormErrorMessage>
+                            )}
+                          </FormControl>
+                        </Stack>
+                      </Stack>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
+              </ModalBody>
+
+              <ModalFooter>{signedButtonMarkup}</ModalFooter>
+            </ModalContent>
+          </Modal>
         </Stack>
       </SimpleGrid>
     </Container>
