81bb4448679be8f344fdc9bca1d18deb3a1dcb45
unknown <lakweensenathilake@gmail.com>
2022-07-27 06:46:44 +0530

    stry-admin routes

--- src/admin/Users/list-users/list-users.page.js
+++ src/admin/Users/list-users/list-users.page.js
@@ -1,24 +1,24 @@
 import UserListTable from "./components/user-list-table";
-import {useEffect, useState} from "@types/react";
+import {useEffect, useState} from "react";
 import {getAllDocFromCollection} from "../../../common/common-action/common-action";
 
 const ListUsersPage = () => {
-    const [users,setUsers] = useState()
+    const [users, setUsers] = useState()
 
-    useEffect(()=>{
+    useEffect(() => {
         getUsers()
-    },[])
+    }, [])
 
-    let columns = ["uid","first_name","last_name",'email',"isNewUser","mobile_number"]
+    let columns = ["uid", "first_name", "last_name", 'email', "isNewUser", "mobile_number"]
 
     const getUsers = async () => {
         let result = await getAllDocFromCollection('accounts')
-        console.log(result,'result')
+        setUsers(result)
     }
 
 
     return (
-        <><UserListTable columns={columns} data={data}/></>
+        <><UserListTable columns={columns} data={users}/></>
     )
 }
 export default ListUsersPage
