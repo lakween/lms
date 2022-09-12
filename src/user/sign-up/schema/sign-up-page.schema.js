bf2b55dedd57cabb0a1d1905192c67eee6c13f8b
lakweensenathilake <lakweensenathilake@gmail.com>
2022-09-12 23:47:34 +0530

    fix

--- src/user/sign-up/schema/sign-up-page.schema.js
+++ src/user/sign-up/schema/sign-up-page.schema.js
@@ -4,7 +4,9 @@ let signSchema = yup.object().shape({
     first_name: yup.string().required('First Name is Required'),
     last_name: yup.string().required('Last Name is Required'),
     email: yup.string().required('Email is required').email('Enter Valid Email Address'),
-    password: yup.string().required('password is required'),
+    password: yup.string().required('password is required').matches(
+        /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/, 'Password must contain at least 8 characters, one uppercase, one number and one special case character'
+    ),
     birthday: yup.date().required('birthday is required'),
     Address: yup.string().required('address is required'),
     School: yup.string().required('school is required'),
