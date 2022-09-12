f48241810579e700f3ee213933a62c8ded11233f
Udaya <unzoysa.un@gmail.com>
2022-09-12 20:29:46 +0530

    speach to talak updated

--- src/user/my-course/components/self-trainning-markup.component.js
+++ src/user/my-course/components/self-trainning-markup.component.js
@@ -1,103 +1,93 @@
 import {
-    Card, CardTitle, Col, Row, ListGroup,
-    ListGroupItem,
-    Badge,
+  Card,
+  CardTitle,
+  Col,
+  Row,
+  ListGroup,
+  ListGroupItem,
+  Badge,
 } from "reactstrap";
-import {FiPaperclip} from "react-icons/fi";
+import { FiPaperclip } from "react-icons/fi";
 import SpeechRecognition, {
-    useSpeechRecognition,
+  useSpeechRecognition,
 } from "react-speech-recognition";
-import {useEffect} from "react";
+import { useEffect } from "react";
+const stringSimilarity = require("string-similarity");
 
-const SelfTrainningMarkupComponent = ({data}) => {
+const SelfTrainningMarkupComponent = ({ data }) => {
+  useEffect(() => {
+    SpeechRecognition.startListening({ continuous: true });
+    console.log("start lisrnting");
+  }, []);
 
-    useEffect(() => {
-        SpeechRecognition.startListening({ continuous: true });
-        console.log("start lisrnting");
-    }, []);
+  const { transcript, resetTranscript } = useSpeechRecognition();
 
-    const { transcript, resetTranscript } = useSpeechRecognition();
+  return (
+    <>
+      {data?.map((item) => (
+        <>
+          <h1 className="mt-4 mb-4 f-25">
+            Your Patrice Sentences are Below <Badge>New</Badge>
+          </h1>
+          <Row className={"mt-2"}>
+            <div className="col-md-12 mt-4 mb-4">
+              <ListGroup>
+                <ListGroupItem href="#" tag="a" key={item.id}>
+                  {item.materialDesc}
+                  <span className="float-end">
+                    {stringSimilarity.compareTwoStrings(
+                      item.materialDesc,
+                      transcript
+                    ) * 100}{" "}
+                    %
+                  </span>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+            <div class="col-lg-12">
+              <div class="form-group">
+                <textarea
+                  id="message"
+                  name="message"
+                  cols="30"
+                  rows="6"
+                  class="form-control"
+                  placeholder="Your Message"
+                  value={transcript}
+                ></textarea>
+              </div>
+            </div>
+            <div class="col-lg-12 mt-2">
+              <button
+                className="btn btn-primary btn-sm mx-2"
+                onClick={SpeechRecognition.startListening({
+                  continuous: true,
+                })}
+              >
+                Start
+              </button>
+              <button
+                className="btn btn-primary btn-sm mx-2"
+                onClick={(e) => {
+                  e.preventDefault();
+                  SpeechRecognition.stopListening();
+                  console.log("listening stops");
+                }}
+              >
+                Stop
+              </button>
+              <button
+                className="btn btn-primary btn-sm mx-2"
+                onClick={resetTranscript}
+              >
+                Clear text
+              </button>
+            </div>
+          </Row>
+        </>
+      ))}{" "}
+    </>
+  );
+};
 
-    return (<>{
-        data?.map((item) => (
-            <>
-                <Row className={'mt-2'}>
-                    <div className="col-md-12 mt-4 mb-4">
-                        <h1 className="mt-4 mb-4 f-25">
-                            Your Patrice Sentences are Below <Badge>New</Badge>
-                        </h1>
-                        <ListGroup>
-                            <ListGroupItem disabled href="#" tag="a">
-                                Sri Lanka is a very beautiful country
-                                <span className="float-end"> 81%</span>
-                            </ListGroupItem>
-                            <ListGroupItem href="#" tag="a">
-                                most Sri Lankan have their favourite games cricket
-                                <span className="float-end"> 81%</span>
-                            </ListGroupItem>
-                            <ListGroupItem href="#" tag="a">
-                                ABC news from the American Television Prime Time news
-                                <span className="float-end"> 81%</span>
-                            </ListGroupItem>
-                            <ListGroupItem href="#" tag="a">
-                                Russia will attack the Ukraine and their military
-                                <span className="float-end">
-                            <button
-                                className="btn btn-primary btn-sm mx-2"
-                                onClick={SpeechRecognition.startListening({
-                                    continuous: true,
-                                })}
-                            >
-                              Start
-                            </button>
-                          </span>
-                                power is very high
-                            </ListGroupItem>
-                        </ListGroup>
-                    </div>
-                    <div class="col-lg-12">
-                        <div class="form-group">
-                        <textarea
-                            id="message"
-                            name="message"
-                            cols="30"
-                            rows="6"
-                            class="form-control"
-                            placeholder="Your Message"
-                            value={transcript}
-                        ></textarea>
-                        </div>
-                    </div>
-                    <div class="col-lg-12 mt-2">
-                        <button
-                            className="btn btn-primary btn-sm mx-2"
-                            onClick={SpeechRecognition.startListening({
-                                continuous: true,
-                            })}
-                        >
-                            Start
-                        </button>
-                        <button
-                            className="btn btn-primary btn-sm mx-2"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                SpeechRecognition.stopListening();
-                                console.log("listening stops");
-                            }}
-                        >
-                            Stop
-                        </button>
-                        <button
-                            className="btn btn-primary btn-sm mx-2"
-                            onClick={resetTranscript}
-                        >
-                            Clear text
-                        </button>
-                    </div>
-                </Row>
-            </>
-        ))
-    } </>)
-}
-
-export default SelfTrainningMarkupComponent
+export default SelfTrainningMarkupComponent;
