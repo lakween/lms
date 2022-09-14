c60c0997fd84a238f025319c8f59d1a1f0843d84
Udaya <unzoysa.un@gmail.com>
2022-09-14 23:20:11 +0530

    fix

--- src/admin/Material/components/material-table.component.js
+++ src/admin/Material/components/material-table.component.js
@@ -2,6 +2,7 @@ import { Col, Row, Table, Button } from "reactstrap";
 import CourseNameCell from "../../Payments/components/course-name.cell";
 import StudentNameCell from "../../Payments/components/student-name-cell";
 import { Link } from "react-router-dom";
+import firebase from "firebase/compat/app";
 
 const MaterialTableComponent = ({
   columns = [],
@@ -9,6 +10,12 @@ const MaterialTableComponent = ({
   setRefetch,
   refetch,
 }) => {
+  const handleClick = async (event, param) => {
+    alert("Material Delete Success");
+    const db = firebase.firestore();
+    await db.collection("materials").doc(param).delete();
+    window.location.reload();
+  };
   return (
     <>
       <Row>
@@ -30,7 +37,25 @@ const MaterialTableComponent = ({
                   <td>{item?.videoName}</td>
                   <td>{item?.videoURL}</td>
                   <td className={"overflow-hidden w-[10vw]"}>
-                    <a href={item?.fileUrl}>View</a>
+                    <Button className=" btn-sm" color="primary btn-sm">
+                      <a href={item?.fileUrl}>View</a>
+                    </Button>
+                    <Button
+                      className="btn-sm mx-2"
+                      color="danger"
+                      onClick={(event) => {
+                        if (
+                          window.confirm(
+                            "Are you sure you wish to delete this item?"
+                          )
+                        ) {
+                          handleClick(event, item.id);
+                        }
+                        this.onCancel(item.id);
+                      }}
+                    >
+                      Delete
+                    </Button>
                   </td>
                 </tr>
               ))}
